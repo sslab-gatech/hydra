6389d147fd4fbca9ec6cf95183886926c6c60cad
Seulbae Kim <squizz617@gmail.com>
2019-08-29 11:06:55 -0400

    create directory through run.py

--- src/prepare_env.sh
+++ src/prepare_env.sh
@@ -4,5 +4,5 @@ wget https://gts3.org/~seulbae/samples.tar.gz
 tar -xvzf samples.tar.gz
 rm samples.tar.gz
 
-mkdir in out seed
+mkdir seed
 ./combined/create_corpus_consistency istat/btrfs-10.istat seed
