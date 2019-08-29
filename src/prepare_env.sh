643dfbae1d63b2789530296c40deb710481ad4bc
Seulbae Kim <squizz617@gmail.com>
2019-08-29 15:26:33 -0400

    change owner

--- src/prepare_env.sh
+++ src/prepare_env.sh
@@ -4,5 +4,7 @@ wget https://gts3.org/~seulbae/samples.tar.gz
 tar -xvzf samples.tar.gz
 rm samples.tar.gz
 
+chown -R $USER samples
+
 mkdir seed
 ./combined/create_corpus_consistency istat/btrfs-10.istat seed
