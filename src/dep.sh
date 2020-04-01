c5d1b0ccc9e45c785796e417d17d33c3237eb130
Seulbae Kim <squizz617@gmail.com>
2020-03-31 22:50:02 -0400

    update dep.sh for ubuntu 18.04

--- src/dep.sh
+++ src/dep.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# ubuntu 16.04
 sudo apt-get -y install cmake libboost-dev libarchive-dev zlib1g-dev \
-	flex bison bc libunistring0 libunistring-dev \
-	python3-zmq python-zmq python3-aiozmq python3-zmq-dbg libzmq3-dev libzmq1 \
-    e2fslibs-dev
+    flex bison bc libunistring0 libunistring-dev \
+    e2fslibs-dev f2fs-tools \
+    python3-zmq python-zmq python3-aiozmq python3-zmq-dbg libzmq3-dev libzmq1 \
+
+#ubuntu 18.04
+sudo apt-get -y install cmake libboost-dev libarchive-dev zlib1g-dev \
+    flex bison bc libunistring2 libunistring-dev \
+    e2fslibs-dev f2fs-tools \
+    python3-zmq python-zmq python3-aiozmq python3-zmq-dbg libzmq3-dev libzmq5 \
