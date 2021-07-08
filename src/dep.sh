22a424934e5bef888a00520572204e767bb94e87
Seulbae Kim <squizz617@gmail.com>
2021-07-08 16:48:02 -0400

    update dependencies for ubuntu 20.04

--- src/dep.sh
+++ src/dep.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-# ubuntu 16.04
-sudo apt-get -y install cmake libboost-dev libarchive-dev zlib1g-dev \
+if [[ $(lsb_release -rs) == "16.04" ]]; then
+  echo "install dependencies for Ubuntu 16.04"
+  sudo apt-get -y install cmake libboost-dev libarchive-dev zlib1g-dev \
     flex bison bc libunistring0 libunistring-dev \
     e2fslibs-dev f2fs-tools \
     python3-zmq python-zmq python3-aiozmq python3-zmq-dbg libzmq3-dev libzmq1 \
 
-#ubuntu 18.04
-sudo apt-get -y install cmake libboost-dev libarchive-dev zlib1g-dev \
+elif [[ $(lsb_release -rs) == "18.04" ]]; then
+  echo "install dependencies for Ubuntu 19.04"
+  sudo apt-get -y install cmake libboost-dev libarchive-dev zlib1g-dev \
     flex bison bc libunistring2 libunistring-dev \
     e2fslibs-dev f2fs-tools \
     python3-zmq python-zmq python3-aiozmq python3-zmq-dbg libzmq3-dev libzmq5 \
+
+elif [[ $(lsb_release -rs) == "20.04" ]]; then
+  echo "install dependencies for Ubuntu 20.04"
+  sudo apt-get -y install cmake libboost-dev libarchive-dev zlib1g-dev \
+    flex bison bc libunistring2 libunistring-dev \
+    e2fslibs-dev f2fs-tools \
+    python3-zmq python3-zmq-dbg libzmq3-dev libzmq5 \
+
+  pip3 install aiozmq
+
+else
+  echo "Ubuntu 16.04 / 18.04 / 20.04 is required"
+
+fi
